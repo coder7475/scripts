9345ee8cc06f3b461245b16535fee3b36b0dfdfe
coder7475 <robiulhossain7475@gmail.com>
2024-11-05 07:57:40 +0600

    feat: path analysis task 3

--- bash_scripts/3_path_analysis.sh
+++ bash_scripts/3_path_analysis.sh
@@ -7,13 +7,16 @@
 # Display the full PATH
 echo "Full PATH:"
 echo $PATH
+echo ""
 
 echo "Directories in PATH:"
 echo $PATH | tr ':' '\n'
+echo ""
 
 # Count and display the number of directories in PATH
-echo "Number of directories in PATH: ..."
-
+echo "Number of directories in PATH: "
+echo $PATH | tr ':' '\n' | wc -l
+echo ""
 # List the first 5 directories in PATH
 echo "First 5 directories in PATH:"
 echo "..."
