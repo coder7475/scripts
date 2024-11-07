a94829eb7f65db68f77e21d892ca2e40e1832034
coder7475 <robiulhossain7475@gmail.com>
2024-11-07 06:48:26 +0600

    feat: solved labex shell exercise 1

---
+++ labex_exercises/1_treasure_map.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+
+LATITUDE=15
+LONGITUDE=25
+
+PACES_NORTH=$((LATITUDE*2))
+PACES_EAST=$((LONGITUDE/2))
+
+# Don't modify the line below
+echo "The treasure is buried $PACES_NORTH paces north and $PACES_EAST paces east from the old oak tree."
