009e7f809fd59b3d527c6b854cb1d564a195b4dc
coder7475 <robiulhossain7475@gmail.com>
2024-11-10 08:21:23 +0600

    exta scripts

---
+++ labex_exercises/0_weather_advisor.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+echo "Enter the current temperature in Celsius: "
+# The 'read' command is used to accept user input
+# It reads a line from standard input and assigns it to the variable 'temp'
+read temp
+echo $temp
+
+# TODO: Implement if statements here to provide weather advice based on the temperature
+# Use the following temperature ranges:
+# Below 0°C: "It's freezing! Wear a heavy coat and gloves."
+# 0°C to 10°C: "It's cold. A warm jacket is recommended."
+# 11°C to 20°C: "It's cool. A light jacket should suffice."
+# Above 20°C: "It's warm. Enjoy the pleasant weather!"
+
