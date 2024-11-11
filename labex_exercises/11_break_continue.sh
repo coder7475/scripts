47f97135a1782aeeca210ddac803b849dfdf5ea4
coder7475 <robiulhossain7475@gmail.com>
2024-11-11 07:55:08 +0600

    learn: continue statement

--- labex_exercises/11_break_continue.sh
+++ labex_exercises/11_break_continue.sh
@@ -9,3 +9,14 @@ for i in {1..10}; do
     fi
     echo $i
 done
+
+echo 
+
+echo "Demonstration of continue (Priting odd numbers):"
+
+for i in {1..10}; do
+    if [ $((i % 2)) -eq 0 ]; then
+        continue
+    fi
+    echo $i
+done
