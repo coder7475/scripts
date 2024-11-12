57b62f4302443356c488e3d667b706dea1932365
coder7475 <robiulhossain7475@gmail.com>
2024-11-12 18:13:52 +0600

    feat: english calcl with shell script

---
+++ labex_exercises/13_ENG_CALC.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+ENGLISH_CALC() {
+  local num1=$1
+  local operation=$2
+  local num2=$3
+  local result
+
+  case $operation in
+    plus)
+      result=$((num1 + num2))
+      echo "$num1 + $num2 = $result"
+      ;;
+    minus)
+      result=$((num1 - num2))
+      echo "$num1 - $num2 = $result"
+      ;;
+    times)
+      result=$((num1 * num2))
+      echo "$num1 * $num2 = $result"
+      ;;
+    *)
+      echo "Invalid operation. Please use 'plus', 'minus', or 'times'."
+      return 1
+      ;;
+  esac
+}
+
+# Test the function
+ENGLISH_CALC 3 plus 5
+ENGLISH_CALC 5 minus 1
+ENGLISH_CALC 4 times 6
+ENGLISH_CALC 2 divide 2 # This should show an error message
