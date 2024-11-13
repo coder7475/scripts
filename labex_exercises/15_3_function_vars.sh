0314689e258df5a89b499614cde39a031657a255
coder7475 <robiulhossain7475@gmail.com>
2024-11-13 07:41:29 +0600

    script: function vars

---
+++ labex_exercises/15_3_function_vars.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+function print_args {
+  echo "Function Name: $0"
+  echo "First Argument: $1"
+  echo "Second Argument: $2"
+  echo "All Arguments: $@"
+  echo "Number of Arguments: $#"
+}
+
+echo "Calling function with two arguments:"
+print_args hello world
+
+echo "Calling function with four arguments:"
+print_args one two three four
