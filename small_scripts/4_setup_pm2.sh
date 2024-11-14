4eb99a48749042721bf7eed2dc81e2abd0aafb7e
coder7475 <robiulhossain7475@gmail.com>
2024-11-14 10:07:42 +0600

    small_script: pm2 setup

--- small_scripts/4_setup_pm2.sh
+++ small_scripts/4_setup_pm2.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
+echo "Installing PM2..."
 sudo npm install -g pm2@latest
+
+echo "Setting up PM2 to start on boot..."
+sudo pm2 startup
+
+echo "Checking PM2 version..."
+sudo pm2 --version
