27425113364c18a104768da91d2d840d1ab28a0a
coder7475 <robiulhossain7475@gmail.com>
2024-11-16 09:46:45 +0600

    script: improve reloading script

--- small_scripts/7_reload_nginx.sh
+++ small_scripts/7_reload_nginx.sh
@@ -1,3 +1,10 @@
 #!/bin/bash
 
-sudo systemctl reload nginx
+# Check nginx config syntax
+if sudo nginx -t; then
+    echo "Nginx config test successful. Reloading..."
+    sudo systemctl reload nginx
+else
+    echo "Nginx config test failed. Not reloading."
+    exit 1
+fi
