9b4c2ab3612855977688a038e9349a8bd8d50ad9
coder7475 <robiulhossain7475@gmail.com>
2024-12-02 12:08:32 +0600

    feat: execute task by tag

--- ansible/p1_list_all_tasks.sh
+++ ansible/p1_list_all_tasks.sh
@@ -2,3 +2,6 @@
 
 # List all tasks contained in a playbook
 ansible-playbook -i inventory playbook.yml --list-tasks
+
+# Listing Playbook Tags
+ansible-playbook -i inventory playbook.yml --list-tags
